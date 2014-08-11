c5164ecc9131c77ab59548e50b5614839985043f
Sebastian Gibb <mail@sebastiangibb.de>
2014-08-11 00:00:00 +0000

    version 0.9

--- R/analyze-functions.R
+++ R/analyze-functions.R
@@ -36,10 +36,11 @@
   }
 
   f <- file(filename, open="rb")
+  on.exit(close(f))
 
   ## first 4 bytes have to be 348 in little endian mode
   ## (384 for ABSciex)
-  endian <- ifelse(readBin(f, integer(), n=1, size=4, endian="little") %in%
+  endian <- ifelse(readBin(f, "integer", n=1, size=4, endian="little") %in%
                    c(348, 384), "little", "big")
 
   ## skip unused entries
@@ -53,8 +54,12 @@
 
   ## skip unused entries
   seek(f, where=40)
-  ## 2 == number of intesity, 3 == ncol (x), 4 == nrow (y)
-  dimensions <- readBin(f, integer(), n=8, size=2, endian=endian)
+  ## 2 == number of intensity, 3 == ncol (x), 4 == nrow (y)
+  ## use as.double() here to avoid integer overflows in calculations like nx*ny
+  ## later
+  ## Thanks to Ken Frankel <kafrankel@gmail.com> for reporting this problem.
+  dimensions <- as.double(readBin(f, "integer", n=8, size=2, endian=endian))
+
   ## We use the size of the t2m file. See .readAnalyzeIntensity for details.
   #ni <- dimensions[2]
   nx <- dimensions[3]
@@ -62,15 +67,15 @@
 
   ## skip unused entries
   seek(f, where=70)
-  datatype <- readBin(f, integer(), n=1, size=2, endian=endian)
-  bitpix <- readBin(f, integer(), n=1, size=2, endian=endian)
+  datatype <- readBin(f, "integer", n=1, size=2, endian=endian)
+  bitpix <- readBin(f, "integer", n=1, size=2, endian=endian)
 
   if (datatype %in% c(2, 4, 8)) {
-    what <- integer()
+    what <- "integer"
   } else if (datatype %in% c(16, 32, 64)) {
-    what <- double()
+    what <- "double"
   } else {
-    what <- raw()
+    what <- "raw"
   }
 
   signed <- datatype == 2
@@ -78,19 +83,18 @@
 
   ## skip unused entries
   seek(f, where=76)
-  pixdim <- readBin(f, double(), n=8, size=4, endian=endian)
+  pixdim <- readBin(f, "double", n=8, size=4, endian=endian)
   ## pixelwidth in mm
   xd <- pixdim[2]
   yd <- pixdim[3]
-
-  close(f)
 
   return(list(nx=nx, ny=ny, xd=xd, yd=yd,
               endian=endian, what=what, signed=signed, size=size))
 }
 
 ## Analyze 7.5 img file
-.readAnalyzeIntensity <- function(filename, header, ni, verbose=FALSE) {
+.readAnalyzeIntensity <- function(filename, header, ni, skip=c(0, 0),
+                                  verbose=FALSE) {
   if (!file.exists(filename)) {
     stop(sQuote(filename), " isn't readable.")
   }
@@ -99,17 +103,32 @@
     message("Reading intensity values from ", sQuote(filename), " ...")
   }
 
+  stopifnot(length(skip) == 2)
+  skip <- skip*header$size
+
   f <- file(filename, open="rb")
+  on.exit(close(f))
   ## header$ni should contain the number of intensity values
   ## because the format specification uses int16 for header$ni it is limited to
   ## 32767 intensity values.
   ## We use the size of the t2m file divided by 4 to find the correct number to
   ## circumvent this size limit.
   ## Thanks to Ken Frankel <kafrankel@gmail.com> for reporting this problem.
-  i <- readBin(f, what=header$what, n=ni*header$nx*header$ny,
-               size=header$size, signed=header$signed, endian=header$endian)
+  i <- vector(mode=header$what, length=ni*header$nx*header$ny)
   dim(i) <- c(ni, header$nx, header$ny)
-  close(f)
+
+  for (y in 1:header$ny) {
+    for (x in 1:header$nx) {
+      ## CAUTION: seek on Windows is possibly buggy; see ?seek for details.
+      ## If there are any bug reports on Windows we maybe have to ignore "skip"
+      ## on Windows (which would disable the mass range/memory saving feature
+      ## completely).
+      seek(f, where=seek(f)+skip[1])
+      i[, x, y] <- readBin(f, what=header$what, n=ni, size=header$size,
+                           signed=header$signed, endian=header$endian)
+      seek(f, where=seek(f)+skip[2])
+    }
+  }
 
   return(i)
 }
@@ -126,8 +145,8 @@
 
   n <- file.info(filename)$size/4
   f <- file(filename, open="rb")
-  m <- readBin(f, what=double(), n=n, size=4, signed=TRUE, endian=header$endian)
-  close(f)
+  on.exit(close(f))
+  m <- readBin(f, what="double", n=n, size=4, signed=TRUE, endian=header$endian)
 
   return(m)
 }
